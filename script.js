14945a54f8c4ca700072dde54cf37dbb5d9b4012
Lyric-Joseph <98564614+RasticRookie@users.noreply.github.com>
2025-11-16 14:11:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var continueButton = document.getElementById("Continue");
 var reStart = document.getElementById("Restart");
-var gameOver = document.getElementById("GameOver");
+var gameOver = document.getElementById("gameOver");
 var button1 = document.getElementById("option1");
 var button2 = document.getElementById("option2");
 var button3 = document.getElementById("option3");
@@ -13,21 +13,4 @@ button2.style.display = "none";
 button3.style.display = "none";
 button4.style.display = "none";
 
-let clickCount = 0;
 
-button1.addEventListener("click", redClick);
-button2.addEventListener("click", blueClick);
-button3.addEventListener("click", cakeClick);
-button4.addEventListener("click", headbandClick);
-continueButton.addEventListener("click", nextPage);
-reStart.addEventListener("click", reStartt);
-gameOver.addEventListener("click", gameOverr);
-
-
-
-if (clickCount == 0) {
-   button1.style.display = "none";
-  button2.style.display = "none";
-  button3.style.display = "none";
-  button4.style.display = "none";
-};
